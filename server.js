8c4b02c66d3daa27c2cd03344e368ec88eb2247d
Meghana <meghanaa15@gmail.com>
2026-07-10 13:59:51 +0530

    Add notification storage and GET endpoint

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const app = express();
 
 const PORT = 5000;
 
+app.use(express.json());
+
+let notifications = [];
+
 app.get("/",(req,res)=>{
     res.send("Welcome to Notification Service API");
 });
+
+app.post("/notifications",(req,res)=>{
+    console.log(req.body);
+
+    res.send("Notifications received");
+});
+
+app.get("/notifications", (req, res) => {
+    res.json(notifications);
+});
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
